57edd25ef971535f0a0ee9bb74dd8d889af17bc3
unknown <lexuandanh1001@gmail.com>
2022-01-06 15:56:55 +0700

    nodejs file first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require('express');
+const app = express();
+const port = 3000;
+const handlebars = require('express-handlebars');
+
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/news', (req, res) => {
+    res.render('newpag');
+});
+app.listen(port);
